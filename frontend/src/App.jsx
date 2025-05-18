b7c48d3b1b7b4003a228fe58b78ff6b995c74c95
yuyefet <yuyefet@g.jct.ac.il>
2025-05-18 12:40:49 +0300

    Merging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,13 @@
-import React from 'react';
-import { Link, Routes, Route, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react'
 
-const App = () => {
-  const navigate = useNavigate();
-
-  const user = JSON.parse(localStorage.getItem('activeUser'));
-
-  if (!user) {
-    navigate('/login');
-    return null; // לא מציג כלום אם לא מחובר
-  }
-
-  const handleLogout = () => {
-    localStorage.removeItem('activeUser');
-    navigate('/login');
-  };
-
+function App() {
+  const API_URL = 'https://jsonplaceholder.typicode.com';
+  const [reqType, setReqType] = useState('users');
   return (
-    <div>
-      <h1>Welcome, {user.name}</h1>
+    <>
+      
+    </>
+  )
+}
 
-      <nav>
-        <button onClick={() => navigate('info')}>Info</button>
-        <button onClick={() => navigate('todos')}>Todos</button>
-        <button onClick={() => navigate('posts')}>Posts</button>
-        <button onClick={() => navigate('albums')}>Albums</button>
-        <button onClick={handleLogout}>Logout</button>
-      </nav>
-
-      <Routes>
-        <Route path="info" element={<div>Info page (coming soon)</div>} />
-        <Route path="todos" element={<div>Todos page (coming soon)</div>} />
-        <Route path="posts" element={<div>Posts page (coming soon)</div>} />
-        <Route path="albums" element={<div>Albums page (coming soon)</div>} />
-      </Routes>
-    </div>
-  );
-};
-
-export default App;
+export default App
