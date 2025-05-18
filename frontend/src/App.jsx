8ef5546581970e20bfd6dfccca7cd4084f30c30e
yuyefet <yuyefet@g.jct.ac.il>
2025-05-18 12:41:42 +0300

    Merging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,42 @@
-import { useState, useEffect } from 'react'
+import React from 'react';
+import { Link, Routes, Route, useNavigate } from 'react-router-dom';
+import Todos from './components/Todos.jsx'
 
-function App() {
-  const API_URL = 'https://jsonplaceholder.typicode.com';
-  const [reqType, setReqType] = useState('users');
+const App = () => {
+  const navigate = useNavigate();
+
+  const user = JSON.parse(localStorage.getItem('activeUser'));
+
+  if (!user) {
+    navigate('/login');
+    return null; 
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('activeUser');
+    navigate('/login');
+  };
+
   return (
-    <>
-      
-    </>
-  )
-}
+    <div>
+      <h1>Welcome, {user.name}</h1>
 
-export default App
+      <nav>
+        <button onClick={() => navigate('info')}>Info</button>
+        <button onClick={() => navigate('todos')}>Todos</button>
+        <button onClick={() => navigate('posts')}>Posts</button>
+        <button onClick={() => navigate('albums')}>Albums</button>
+        <button onClick={handleLogout}>Logout</button>
+      </nav>
+
+      <Routes>
+        <Route path="info" element={<div>Info page (coming soon)</div>} />
+        <Route path="todos" element={<Todos/>} />
+        <Route path="posts" element={<div>Posts page (coming soon)</div>} />
+        <Route path="albums" element={<div>Albums page (coming soon)</div>} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
